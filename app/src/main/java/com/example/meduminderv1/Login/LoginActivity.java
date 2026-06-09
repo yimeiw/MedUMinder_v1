77c3f7948cee7fe3b054408d60ef5c8e8b849e78
yimeiw <yimei.winata@binus.ac.id>
2026-06-09 16:06:01 +0800

    update login signup

--- app/src/main/java/com/example/meduminderv1/Login/LoginActivity.java
+++ app/src/main/java/com/example/meduminderv1/Login/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.meduminderv1.Login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.meduminderv1.R;
+import com.example.meduminderv1.SignUp.SignUpActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,13 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        signUpButton = findViewById(R.id.sign_up);
+
+        signUpButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SignUpActivity.class);
+            startActivity(intent);
         });
     }
 }
