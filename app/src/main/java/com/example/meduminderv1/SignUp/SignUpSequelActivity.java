774d1e505aca472e2b5682296eccb2b47caa9521
yimeiw <yimei.winata@binus.ac.id>
2026-06-09 16:01:41 +0800

    update sign up login 2

---
+++ app/src/main/java/com/example/meduminderv1/SignUp/SignUpSequelActivity.java
@@ -0,0 +1,26 @@
+package com.example.meduminderv1.SignUp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.meduminderv1.R;
+
+public class SignUpSequelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up_sequel);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
