682060113cffadd3de576947f33e6e2f2e42685c
Clatan <149095889+Clatan@users.noreply.github.com>
2026-06-12 16:34:50 +0700

    log tp blom cb run

--- app/src/main/java/com/example/meduminderv1/Log/LogActivity.java
+++ app/src/main/java/com/example/meduminderv1/Log/LogActivity.java
@@ -1,4 +1,4 @@
-package com.example.meduminderv1;
+package com.example.meduminderv1.Log;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.meduminderv1.R;
 
 public class LogActivity extends AppCompatActivity {
 
